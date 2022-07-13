f4fe3e3ba2168f1c829c280b69ef59bee0e4b84d
Corentin Deret <corentin.deret@payfit.com>
2022-07-13 14:02:04 +0200

    qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,6 +5,15 @@ c.new_instance_open_target = 'window'
 c.downloads.position = 'bottom'
 c.spellcheck.languages = ['en-US']
 
+config.unbind("<ctrl+tab>")
+config.bind("<ctrl+tab>", "tab-next")
+config.bind("<ctrl+shift+tab>", "tab-prev")
+
+c.input.insert_mode.auto_load = True
+c.auto_save.session = True
+c.downloads.location.directory = "$HOME/Downloads/"
+
+c.url.default_page = "https://google.com"
 
 c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
 config.bind('<Shift-Left>', 'back')
